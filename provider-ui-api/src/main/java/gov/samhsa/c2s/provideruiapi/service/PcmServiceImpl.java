1cc589d76d9f509afadd6c1d645db23a588d3554
Shruti Rao <shruti.rao@feisystems.com>
2017-06-06 16:06:37 -0400

    Add placeholders to assert the current provider is authorized to manage consents for mrn
    #14045:As a developer, I will have implemented the backend to support - Provider managing patient's consents workflow

--- provider-ui-api/src/main/java/gov/samhsa/c2s/provideruiapi/service/PcmServiceImpl.java
+++ provider-ui-api/src/main/java/gov/samhsa/c2s/provideruiapi/service/PcmServiceImpl.java
@@ -25,11 +25,13 @@ public class PcmServiceImpl implements PcmService {
 
     @Override
     public void saveProviders(String mrn, IdentifiersDto providerIdentifiersDto) {
+        //TODO: Assert the current provider is authorized to manage consents for mrn
         pcmClient.saveProviders(mrn, providerIdentifiersDto);
     }
 
     @Override
     public void deleteProvider(String mrn, Long providerId) {
+        //TODO: Assert the current provider is authorized to manage consents for mrn
         pcmClient.deleteProvider(mrn, providerId);
     }
 
@@ -40,11 +42,13 @@ public class PcmServiceImpl implements PcmService {
 
     @Override
     public PageableDto<Object> getConsents(String mrn, Optional<Long> purposeOfUse, Optional<Long> fromProvider, Optional<Long> toProvider, Integer page, Integer size) {
+        //TODO: Assert the current provider is authorized to manage consents for mrn
         return pcmClient.getConsents(mrn, purposeOfUse, fromProvider, toProvider, page, size);
     }
 
     @Override
     public Object getConsent(String mrn, Long consentId, String format) {
+        //TODO: Assert the current provider is authorized to manage consents for mrn
         return pcmClient.getConsent(mrn, consentId, format);
     }
 }
