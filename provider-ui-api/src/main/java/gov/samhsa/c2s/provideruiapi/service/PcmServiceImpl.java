368d23a35d8a219b5f77caac39f276b12dab37bf
Shruti Rao <shruti.rao@feisystems.com>
2017-06-14 14:00:36 -0400

    Remove TODOs
    #14045:As a developer, I will have implemented the backend to support - Provider managing patient's consents workflow

--- provider-ui-api/src/main/java/gov/samhsa/c2s/provideruiapi/service/PcmServiceImpl.java
+++ provider-ui-api/src/main/java/gov/samhsa/c2s/provideruiapi/service/PcmServiceImpl.java
@@ -29,13 +29,11 @@ public class PcmServiceImpl implements PcmService {
 
     @Override
     public void saveProviders(String mrn, IdentifiersDto providerIdentifiersDto) {
-        //TODO: Assert the current provider is authorized to manage consents for mrn
         pcmClient.saveProviders(mrn, providerIdentifiersDto);
     }
 
     @Override
     public void deleteProvider(String mrn, Long providerId) {
-        //TODO: Assert the current provider is authorized to manage consents for mrn
         pcmClient.deleteProvider(mrn, providerId);
     }
 
@@ -46,19 +44,16 @@ public class PcmServiceImpl implements PcmService {
 
     @Override
     public PageableDto<Object> getConsents(String mrn, Integer page, Integer size) {
-        //TODO: Assert the current provider is authorized to manage consents for mrn
         return pcmClient.getConsents(mrn, page, size);
     }
 
     @Override
     public Object getConsent(String mrn, Long consentId, String format) {
-        //TODO: Assert the current provider is authorized to manage consents for mrn
         return pcmClient.getConsent(mrn, consentId, format);
     }
 
     @Override
     public void saveConsent(String mrn, ConsentDto consentDto, Locale locale) {
-        //TODO: Assert the current provider is authorized to manage consents for mrn
         // Get current user authId
         String createdBy = jwtTokenExtractor.getValueByKey(JwtTokenKey.USER_ID);
 
@@ -67,8 +62,6 @@ public class PcmServiceImpl implements PcmService {
 
     @Override
     public void updateConsent(String mrn, Long consentId, ConsentDto consentDto) {
-        //TODO: Assert the current provider is authorized to manage consents for mrn
-
         // Get current user authId
         String lastUpdatedBy = jwtTokenExtractor.getValueByKey(JwtTokenKey.USER_ID);
         pcmClient.updateConsent(mrn, consentId, consentDto, lastUpdatedBy);
@@ -77,8 +70,6 @@ public class PcmServiceImpl implements PcmService {
 
     @Override
     public void deleteConsent(String mrn, Long consentId) {
-        //TODO: Assert the current provider is authorized to manage consents for mrn
-
         // Get current user authId
         String lastUpdatedBy = jwtTokenExtractor.getValueByKey(JwtTokenKey.USER_ID);
         pcmClient.deleteConsent(mrn, consentId, lastUpdatedBy);
