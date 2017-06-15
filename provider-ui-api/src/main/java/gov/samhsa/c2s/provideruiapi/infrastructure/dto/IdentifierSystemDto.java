66578de7115b7cb8d9f35b676dd959c941afde07
burcakulug <burcak.ulug@feisystems.com>
2017-06-15 16:48:45 -0400

    Improve identifier system selection logic
    #14044:As a developer I will have implemented backend to support - Provider Managing patient record Workflow

--- provider-ui-api/src/main/java/gov/samhsa/c2s/provideruiapi/infrastructure/dto/IdentifierSystemDto.java
+++ provider-ui-api/src/main/java/gov/samhsa/c2s/provideruiapi/infrastructure/dto/IdentifierSystemDto.java
@@ -2,10 +2,16 @@ package gov.samhsa.c2s.provideruiapi.infrastructure.dto;
 
 import lombok.Data;
 
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @Data
 public class IdentifierSystemDto {
     private String system;
     private String display;
     private String oid;
-    private boolean systemGenerated;
+    @Valid
+    private Map<String, List<RequiredIdentifierSystem>> requiredIdentifierSystemsByRole = new HashMap<>();
 }
